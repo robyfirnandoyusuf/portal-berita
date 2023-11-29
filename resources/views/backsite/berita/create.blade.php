60859ae802725fa9acdede744052f6cd5943080e
Farrelabia <farrelabia2@gmail.com>
2023-11-29 16:57:09 +0700

    help summernote

--- resources/views/backsite/berita/create.blade.php
+++ resources/views/backsite/berita/create.blade.php
@@ -11,12 +11,13 @@
                                 <div class="mb-3">
                                     <div class="card-header card-header-text" data-background-color="rose">
                                         <h4><a href="{{ route('backsite.berita.index') }}"><i
-                                            class="material-icons">view_list</i>
-                                        </a></h4>
+                                                    class="material-icons">view_list</i>
+                                            </a></h4>
                                     </div>
                                     <h5 class="">Upload Gambar</h5>
                                 </div>
-                                <form action="{{ route('backsite.berita.upload') }}" method="post" enctype="multipart/form-data" class="dropzone" id="myDropzone">
+                                <form action="{{ route('backsite.berita.upload') }}" method="post"
+                                    enctype="multipart/form-data" class="dropzone" id="myDropzone">
                                     @csrf
                                 </form>
                             </div>
@@ -44,22 +45,22 @@
                                 <div class="row">
                                     <label class="col-sm-2 label-on-left">Kategori</label>
                                     <div class="col-sm-10 checkbox-radios">
-                                        <select class="form-control js-example-tokenizer" multiple="multiple" name="kategori">
+                                        <select class="form-control js-example-tokenizer" multiple="multiple"
+                                            name="kategori">
                                             @foreach ($kategoris as $kategori)
-
-                                            <option>{{  $kategori->kategori }}</option>
+                                                <option>{{ $kategori->kategori }}</option>
                                             @endforeach
 
                                         </select>
-                                </div>
-                                <div class="row">
-                                    <label class="col-sm-2 label-on-left"></label>
-                                    <div class="col-sm-10">
-                                        <div class="form-group label-floating is-empty">
-                                            <button type="submit" class="btn btn-fill btn-rose">Kirim</button>
+                                    </div>
+                                    <div class="row">
+                                        <label class="col-sm-2 label-on-left"></label>
+                                        <div class="col-sm-10">
+                                            <div class="form-group label-floating is-empty">
+                                                <button type="submit" class="btn btn-fill btn-rose">Kirim</button>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
                             </form>
                         </div>
                     </div>
@@ -67,45 +68,56 @@
             </div>
         </div>
     </div>
-
 @endsection
 
 @section('script')
     {{-- Dropzone --}}
 
-  <script>
-      $(document).ready(function() {
-        $('#summernote').summernote();
-      });
-  </script>
-  <script>
-      $(".js-example-tokenizer").select2({
-          tags: true,
-          tokenSeparators: [',', ' ']
-      })
-      </script>
-      <script type="text/javascript">
-    Dropzone.options.myDropzone =
-    {
-      maxFilesize: 12,
-      renameFile:function(file)
-      {
-          var img = new Gambar();
-          var time = img.getTime();
-          return time+file.name;
-      },
-      acceptedFiles: ".jpeg",".jpg",".png",".gif",
-      addRemoveLinks: true,
-      timeout: 5000,
-      success: function(file,response){
-          console.log(response);
-      },
-      error: function(file,response){
-          return false;
-      }
+    <script>
+        $(document).ready(function() {
+            $('#summernote').summernote({
+                toolbar: [
+                    ['style', ['style']],
+                    ['font', ['bold', 'underline', 'clear']],
+                    ['fontname', ['fontname']],
+                    ['color', ['color']],
+                    ['para', ['ul', 'ol', 'paragraph']],
+                    ['table', ['table']],
+                    ['insert', ['link', 'picture', 'video']],
+                    ['view', ['fullscreen', 'codeview']],
+                ]
+            });
+        });
+    </script>
+    <script>
+        $(".js-example-tokenizer").select2({
+            tags: true,
+            tokenSeparators: [',', ' ']
+        })
+    </script>
+    <script type="text/javascript">
+        Dropzone.options.myDropzone = {
+            maxFilesize: 12,
+            renameFile: function(file) {
+                var img = new Gambar();
+                var time = img.getTime();
+                return time + file.name;
+            },
+            acceptedFiles: ".jpeg",
+            ".jpg",
+            ".png",
+            ".gif",
+            addRemoveLinks: true,
+            timeout: 5000,
+            success: function(file, response) {
+                console.log(response);
+            },
+            error: function(file, response) {
+                return false;
+            }
 
-    };
-  </script>
+        };
+    </script>
 @endsection
 <!-- include libraries(jQuery, bootstrap) -->
 <!-- include summernote css/js -->
